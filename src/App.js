b7f0b9aacb15ab80d7307623aeb67bfa62aabec9
farhanirsyad54 <farhanirsyad54@gmail.com>
2021-12-10 13:27:27 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Formulir from "./Component/Form/Formulir";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div>
+      <Formulir />
+    </div>
+  );
+}
+
+export default App;
